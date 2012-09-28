4c756bc7d32fa01f698b08e96766533afd579ab9
omegatakuma <tooth0819@gmail.com>
2012-09-28 21:17:58 +0900

    Update main.scm

--- main.scm
+++ main.scm
@@ -2,11 +2,13 @@
 (use net.twitter)
 (use sxml.sxpath)
 
+(define user "@自分の名前")
+
 (define *cred* (make <twitter-cred>
 					 :consumer-key "consumer-key"
-					 :consumer-secret "consumer-secret"
-					 :access-token "access-token"
-					 :access-token-secret "access-token-secret"))
+					 :consumer-secret "consumer-key"
+					 :access-token "consumer-key"
+					 :access-token-secret "consumer-key"))
 
 (define (user-get user-id)
   (let ((sxml (twitter-user-show/sxml *cred* :screen-name user-id)))
@@ -30,19 +32,16 @@
 	  (else (loop (cdr lst) (cons (car lst) result))))))
 
 (define (main args)
-  (display "user> ")
-  (flush)
-  (let* ((user-id (x->string (read)))
-		 (file-name (string-append "./."user-id))
-		 (result (user-get user-id))
+  (let* ((file-name (string-append "./."(cadr args)))
+		 (result (user-get (cadr args)))
 		 (solve (if (file-exists? file-name)
 				  (let ((lst (with-input-from-file file-name (pa$ read))))
 					`(,(map (lambda(x y)(- (x->number x) (x->number y)))(car result)(car lst)) 
-							,(let ((hoge (lst? (map (lambda(x)(x->number x)) (sort (cadr lst)))
-											   (map (lambda(x)(x->number x)) (sort (cadr result))))))
-							  (if (null? hoge)
-								'()
-								(map(lambda(x)(user-show x))hoge)))))
+					   ,(let ((hoge (lst? (map (lambda(x)(x->number x)) (sort (cadr lst)))
+										  (map (lambda(x)(x->number x)) (sort (cadr result))))))
+						  (if (null? hoge)
+							'()
+							(map(lambda(x)(user-show x))hoge)))))
 				  '((0 0 0 0) ()))))
 	(print "ツイート数: "(list-ref (car result) 0))
 	(print "  変化: "(list-ref (car solve) 0))
@@ -54,6 +53,8 @@
 	(print "  変化: "(list-ref (car solve) 3))
 	(print "--------詳細--------")
 	(if (null? (cadr solve))
-			 (print "特になし")
-			 (print (string-join (map(lambda(x)(string-append "@"(car x)))(cadr solve)) " ")"にリムられた"))
+	  (print "特になし")
+	  (let ((message (string-append (string-join (map(lambda(x)(string-append "@"(car x)))(cadr solve)) " ")"にリムられた")))
+		(print message)
+		(twitter-update/sxml *cred* (string-append "D " user " " message))))
 	(with-output-to-file file-name (pa$ print result) :if-exists :overwrite :if-does-not-exist :create)))
